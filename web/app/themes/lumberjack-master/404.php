42360d197b58050e28f4319d9cc26498e06023d5
cytronn <arnaud.villani@gmail.com>
2017-03-06 13:40:51 +0100

    first commit

---
+++ web/app/themes/lumberjack-master/404.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * Methods for TimberHelper can be found in the /functions sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+use Timber\Timber;
+
+$context = Timber::get_context();
+
+$context['wp_title'] = 'ta race';
+$context['pageHeading'] = ['title' => 'Page Not Found'];
+$context['content'] = 'We\'re sorry but the page you\'re looking for can\'t be foundation.';
+
+Timber::render(['404.twig', 'generic-page.twig'], $context);
